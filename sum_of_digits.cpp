4d15295e305a395357b95d935398169404ee379f
ozm022 <137074299+ozm022@users.noreply.github.com>
2023-06-20 13:29:09 +0300

    some changes

--- sum_of_digits.cpp
+++ sum_of_digits.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 
+// this func will return the sum of the digits of the int given
 int sum_of_digits(int num_to_check) {
 	if(num_to_check < 10) {
 		return num_to_check;
 	} else {
-		return num_to_check % 10 + sum_of_digits((num_to_check - num_to_check % 10) / 10);
+		return (num_to_check % 10 + 
+				sum_of_digits(num_to_check / 10));
 	}
 }
 
-int main(int argc, char const *argv[])
-{	
+int main(int argc, char const *argv[]) {	
 	if(argc != 2) {
 		std::cout << "./sum_of_digits <YOUR_INT>" << std::endl;
+		return 1;
 	} else {
 		std::cout << "The summary of number digits is: " << sum_of_digits(atoi(argv[1])) << std::endl;
 		return 0;
 	}
-
 }
