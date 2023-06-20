3a1a995537c39b25dad9f950b1dc4de8772c028d
ozm022 <137074299+ozm022@users.noreply.github.com>
2023-06-20 14:19:09 +0300

    some more changes

--- sum_of_digits.cpp
+++ sum_of_digits.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// this func will return the sum of the digits of the int given
+/// this func will return the sum of the digits of the int given
 int sum_of_digits(int num_to_check) {
 	if(num_to_check < 10) {
 		return num_to_check;
@@ -10,6 +10,7 @@ int sum_of_digits(int num_to_check) {
 	}
 }
 
+/// func will take exactly one intput from the user and use sum_of_digits on him
 int main(int argc, char const *argv[]) {	
 	if(argc != 2) {
 		std::cout << "./sum_of_digits <YOUR_INT>" << std::endl;
